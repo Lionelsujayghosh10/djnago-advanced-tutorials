37b601a4f85b943cd223ad14c5425250c6e4a2fc
Lionelsujayghosh10 <ghoshsujay95@gmail.com>
2020-04-10 19:14:46 +0530

    Class delete and Class pagination

--- ClassSection/urls.py
+++ ClassSection/urls.py
@@ -8,5 +8,6 @@ from . import views
 
 urlpatterns = [
     path('createclass', views.create_class, name="create_class"),
-    path('listclass', views.list_class, name="list_class")
+    path('listclass', views.list_class, name="list_class"),
+    path('deleteclass', views.delete_class, name="delete_class")
 ]
--- ClassSection/views.py
+++ ClassSection/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .form import CreateClassForm
 from .models import Classes
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -33,6 +34,46 @@ def create_class(request):
 @login_required
 def list_class(request):
     try:
-        pass
+        # class_list = Classes.objects.filter(deleted__isnull=True)
+        class_list = Classes.objects.all()
+        paginator = Paginator(class_list, 10)
+        page = request.GET.get('page')
+        class_details = paginator.get_page(page)
+        return render(request, 'classlist.html', {'class_list': class_details})
     except Exception as e:
         return render(request, 'createclass.html', { 'error' : e})
+
+
+
+@login_required
+def delete_class(request):
+    try:
+        if request.method == 'POST':
+            if request.POST.get('class_id') is not None:
+                try:
+                    class_exists = Classes.objects.get(pk=request.POST.get('class_id'))
+                    class_exists.delete()
+                    data = {
+                        'success' : 'successfully done.'
+                    }
+                    return JsonResponse(data)
+                except ObjectDoesNotExist:
+                    data = {
+                        'error' : 'Invalid class id.'
+                    }
+                    return JsonResponse(data)
+            else:
+                data = {
+                    'error' : 'class Id required.'
+                }
+                return JsonResponse(data)  
+        else:
+            data = {
+                'error' : 'Unexcepted error ocurred.'
+            }
+            return JsonResponse(data)
+    except Exception as e:
+        data = {
+            'error' : 'Unexcepted error ocurred.'
+        }
+        return JsonResponse(data)
