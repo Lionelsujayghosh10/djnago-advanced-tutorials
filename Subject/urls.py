6062491fcbab4a85e90cc6ceeb303e181a1be378
Lionelsujayghosh10 <ghoshsujay95@gmail.com>
2020-04-16 12:04:37 +0530

    search into assign subject

--- Subject/urls.py
+++ Subject/urls.py
@@ -9,5 +9,6 @@ from . import views
 urlpatterns = [
     path('createsubject', views.create_subject, name="create_subject"),
     path('createassignsubject', views.assign_subject, name="assign_subject"),
-    path('subjectassignlist', views.assign_subject_list, name="assign_subject_list")
+    path('subjectassignlist', views.assign_subject_list, name="assign_subject_list"),
+    path('searchassignsubect', views.search_assign_subject, name="search_assign_subject")
 ]
--- Subject/views.py
+++ Subject/views.py
@@ -5,6 +5,7 @@ from .models import Subject, AssignSubject
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from ClassSection.models import Classes, Section
+from itertools import chain
 
 
 @login_required
@@ -73,4 +74,47 @@ def assign_subject_list(request):
         assign_subject_list = paginator.get_page(page)
         return render(request, 'subject/assignsubjectlist.html', {'assign_subject_list': assign_subject_list})
     except Exception as e:
-        return HttpResponse(e)
+        return render(request, 'subject/assignsubjectlist.html', {'error': e})
+
+
+@login_required
+def search_assign_subject(request):
+    try:
+        if request.method == 'GET':
+            if request.GET.get('table_search') is None or request.GET.get("table_search").strip() == '':
+                return redirect('assign_subject_list')
+            else:
+                # classes = Classes.objects.filter(class_name__contains=request.GET.get('table_search'))
+                # sections = Section.objects.filter(section_name__contains=request.GET.get('table_search'))
+                # subject = Subject.objects.filter(subject_name__contains=request.GET.get('table_search'))
+                search_data = AssignSubject.objects.select_related('classes').select_related('section').select_related('subject').filter(classes__class_name__contains=request.GET.get('table_search'),section__section_name__contains=request.GET.get('table_search'), subject__subject_name__contains=request.GET.get('table_search'))
+                # search_data = list(chain(classes, sections, subject,assign_subject_list))
+
+
+
+                # from django.db.models import Q
+
+                # query = Q(first_name='mark')
+                # query.add(Q(email='mark@test.com'), Q.OR)
+                # query.add(Q(last_name='doe'), Q.AND)
+
+                # queryset = User.objects.filter(query)
+                # This ends up with a query like :
+
+                # (first_name = 'mark' or email = 'mark@test.com') and last_name = 'doe'
+
+
+
+
+
+                search_data =  AssignSubject.objects.select_related('classes').select_related('section').select_related('subject')
+                search_data_filter = search_data.filter(subject__subject_name__contains=request.GET.get('table_search')) | search_data.filter(section__section_name__contains=request.GET.get('table_search')) | search_data.filter(classes__class_name__contains=request.GET.get('table_search'))
+                paginator = Paginator(search_data_filter, 10)
+                page = request.GET.get('page')
+                search_data = paginator.get_page(page)
+                return render(request, 'subject/assignsubjectlist.html', {'assign_subject_list': search_data, 'search_data' : request.GET.get('table_search')})
+        else:
+            return redirect('assign_subject_list')
+    except Exception as e:
+        print(e)
+        return redirect('assign_subject_list')
